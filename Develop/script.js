dcd508bad1af2753b59ca3c75dedd339ece016b4
Alan Hornbaker <alanhornbaker@gmail.com>
2023-01-18 17:48:06 -0600

    3rd commit. Fleshed out README.md, Trimmed comments from html.

--- Develop/script.js
+++ Develop/script.js
@@ -10,21 +10,43 @@ $(function () {
   var unixFormatDate = dayjs.unix(unix).format("M/D/YYYY");
   $("#currentDay").text(unixFormatDate);
 
-  //2.1 Calling the unix variable to define the current hour.
-  var unixFormatTime = dayjs.unix(unix).format("h");
-  //console.log(unixFormatTime); Testing the variable to see if "h" changes with the change of the hour.  It does, so now the variable unixFormatTime can be used to define the past, present, and future classes.
+  //2. Calling the unix variable to define the current hour.
+  //   var unixFormatTime = dayjs.unix(unix).format("h");
+  //   console.log(unixFormatTime);
 
-  //2.2: Use the unixFormatTime to apply the past, present, or future class to the time blocks. I think I need to use an if statement to compare the id of each time block to the unixFormatTime value.
-  if () {
-    
-  }
-  //
+  //Testing the variable to see if "h" changes with the change of the hour.  It does, so now the variable unixFormatTime can be used to define the past, present, and future classes.
+
+  var currentHour = dayjs().hour();
+  //console.log(currentHour);
+  //Deciding to use currentHour instead of my unix-driven variable b/c it displays the result in 24-hour formot.
+
+  //2.2: Use the unixFormstTime or currentHour variables to apply the past, present, or future class to the time blocks. I think I need to use an if statement to compare the id of each time block to the unixFormatTime value.
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  //   if () {
+
+  //   }
+
+  //If currentHour > div id, class = "row time-block past"
+  //If currentHour == div id, class = "row time-block present"
+  //if currentHour < div id, class = "row time-block future"
+
+  var divId = $("#div id");
+  // if (currentHour.isEqual(divId)) {
+  //   set class = "row time-block present";
+
+  //// if (currentHour.isGreater(divId)) {
+  //   set class = "row time-block past";
+
+  // if (currentHour.isLess(divId)) {
+  //   set class = "row time-block future";
+
+  //3.  Click Events and Saving Events in Local Storage
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -36,23 +58,13 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  var saveBtnEl = $("saveBtn");
+
+  //saveBtnEl.on("click", function () {});
+
+  saveBtnEl.click(function () {
+    var inputValue = $("#textarea").val();
+    localStorage.setItem("inputValue", inputValue);
+  });
 });
-
-//Import statement
-//import * as Day from "dayjs";
-//Example code from ChatGPT
-// const currentDay = new Day.js(); // gets the current date and time
-// const startOfWorkDay = new Day.js()
-//   .set("hour", 9)
-//   .set("minute", 0)
-//   .set("second", 0); // sets the start of the work day to 9:00 AM
-// const endOfWorkDay = new Day.js()
-//   .set("hour", 17)
-//   .set("minute", 0)
-//   .set("second", 0); // sets the end of the work day to 5:00 PM
-
-// if (currentDay.isBetween(startOfWorkDay, endOfWorkDay)) {
-//   console.log("It is currently within the work day.");
-// } else {
-//   console.log("It is currently outside of the work day.");
-// }
--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # Module-5-Project_Scheduler
 
 ## Description
+
+The motivation for this project was the need to plan out your workday to improve time management and general productivity. It solves the problem of getting distracted and/or spending way more time than you meant on one task that you had previously only delegated a little time to.
+
+## User Story & Acceptance Criteria
+
+User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+WHEN I click into a time block
+THEN I can enter an event
+
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+
+WHEN I refresh the page
+THEN the saved events persist
+
+## What I learned, Where I Struggled
+
+In this project, I learned how to use day.js to retreive the current date and time. Then I learned how to insert that information into the DOM/html and get it to display on the webpage.
+
+In general, I learned a lot about how jquery/day.js interact and funciton with the DOM/html, and how to get them to work together; how to reference the right spots/parts of code between the two documents.
+
+One thing I struggled with was being unsure about the language syntax, and how to write interactions between the Dom and the the js. For example, in section 2 of the js file, I'm pretty sure I have the right psuedo-code explaining how I want to append the div class depending on how the id value compares with currentHour. However, I'm struggling with the syntax to actually append that to execute correctly.
+
+Similarly, there must be some bug in the syntax of section 3 of the js script, with the click events and saving to local storage.
+
+## TODO for the Future
+
+1. Finish section 2 of the js: functional past/present/future
+2. Finish section 3 of the js: click events and saving input to local storage
